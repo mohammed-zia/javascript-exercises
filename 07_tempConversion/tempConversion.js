2b87d4139e6c47121dd6a42577e2f50f449c6e1d
Mohammed Zia <mohammed-zia@outlook.com>
2022-04-10 11:24:41 +0100

    Ex 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(tempf) {
+  tempc = ((tempf - 32) * (5/9))
+  return Number(tempc.toFixed(1))
 };
 
-const ctof = function() {
-
+const ctof = function(tempc) {
+  tempf = ((tempc * (9/5)) + 32)
+  return Number(tempf.toFixed(1))
 };
 
 // Do not edit below this line
