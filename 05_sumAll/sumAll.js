db39e74c225e38cbd857c38e6a65bf89ee692792
Mohammed Zia <mohammed-zia@outlook.com>
2022-04-10 11:10:42 +0100

    Ex 5 added

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    list=[];
+    if (start > end){
+        oldEnd = end
+        end = start
+        start = oldEnd
+    }
+    if (start < 0 || end < 0){
+        return 'ERROR'
+    }
+    if (typeof(start)!='number' || typeof(end)!='number'){
+        return 'ERROR'
+    }
+    for (i = start; i>=start && i<=end; i++){
+        list.push(i)
+    }
+    sum=0
+    for (num of list){
+        sum=sum+num
+    }
+    return sum
 };
 
 // Do not edit below this line
