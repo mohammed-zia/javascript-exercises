e4c63c62c9cf9fd5c2ff02283f4e4e8b3e6e5c16
Mohammed Zia <mohammed-zia@outlook.com>
2022-04-16 19:50:25 +0100

    Ex 8)

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
+const add = function(a,b) {
+	return a + b
+};
+
+const subtract = function(a,b) {
+	return a - b
+};
+
+const sum = function(arr) {
+  let sum = arr.reduce(function(a,b){
+    return a+b;
+  }, 0);
+  return sum
+};
+
+const multiply = function(arr) {
+  let total = 0;
+  for(let i = 0; i<arr.length; i++){
+    if(i === 0){
+      total = arr[i]
+    }
+    else{
+      total *= arr[i]
+    }
+  }
+  return total
+};
+
+const power = function(a,b) {
+  return a ** b
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(a) {
+  total = 0
+  for(i = 0; i <= a; i++){
+    if(i === 0){
+      total = 1
+    }
+    else{
+      total = i * total
+    }
+  }
+  return total
 };
 
 // Do not edit below this line
