aa9384d52aab3ed4b8a3f3370b0c261c9dce6800
Mohammed Zia <mohammed-zia@outlook.com>
2022-04-16 20:08:24 +0100

    Ex 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase().replace(/[^\w\s]|_/g, "")
+         .replace(/\s+/g, " ");
+    strNoSpace = str.split(' ').join("")
+    strList = strNoSpace.split("")
+    revStr = strNoSpace.toLowerCase().split("").reverse().join(""); 
+    if(strNoSpace === revStr){
+        return true;
+    }
+    else{
+        return false;
+    }
 };
 
 // Do not edit below this line
